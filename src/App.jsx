36db0509447409334731be89f54772bc2f68f2a8
rifkinofianto <rifkinofianto1927@gmail.comgit config --global user.email rifkinofianto1927@gmail.com>
2024-09-12 20:13:59 +0700

    first update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Dashboard from './page/Dashboard';
 import Product from './page/Product';
-import Login from './page/LoginPage';
+import LoginPage from './page/LoginPage';
 function App() {
 
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login/>} />
+        <Route path="/" element={<LoginPage/>} />
         <Route path="/dashboard" element={<Dashboard page = "dashboard"/>} />
         <Route path='/product' element={<Product page = "product"/>} />
       </Routes>
